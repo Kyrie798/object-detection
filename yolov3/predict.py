d12cc0e8fe5b9c0dbe633e6092f200b9e172c2bf
Kyrie <56728472+Kyrie798@users.noreply.github.com>
2023-05-19 09:59:04 +0800

    Update predict.py

--- yolov3/predict.py
+++ yolov3/predict.py
@@ -14,8 +14,7 @@ if __name__ == "__main__":
 
     yolo = YOLO()
 
-    video_save_path = "video_detect.mp4"
-    camera_save_path = "camera_detect.mp4"
+    video_save_path = "detect.mp4"
     video_fps = 25.0
 
     if opt.mode == "image":
@@ -67,52 +66,5 @@ if __name__ == "__main__":
         capture.release()
         if video_save_path!="":
             print("Save processed video to the path :" + video_save_path)
-            out.release()
-        cv2.destroyAllWindows()
-        
-        
-    if opt.mode == "camera":
-        capture = cv2.VideoCapture(0)
-        if camera_save_path !="":
-            fourcc = cv2.VideoWriter_fourcc(*'XVID')
-            size = (int(capture.get(cv2.CAP_PROP_FRAME_WIDTH)), int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-            out = cv2.VideoWriter(camera_save_path, fourcc, video_fps, size)
-
-        ref, frame = capture.read()
-
-        fps = 0.0
-        while(True):
-            t1 = time.time()
-            # 读取某一帧
-            ref, frame = capture.read()
-            if not ref:
-                break
-            # 格式转变，BGRtoRGB
-            frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-            # 转变成Image
-            frame = Image.fromarray(np.uint8(frame))
-            # 进行检测
-            frame = np.array(yolo.detect_image(frame))
-            # RGBtoBGR满足opencv显示格式
-            frame = cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
-            
-            fps  = ( fps + (1./(time.time()-t1)) ) / 2
-            # print("fps= %.2f"%(fps))
-            frame = cv2.putText(frame, "fps= %.2f"%(fps), (0, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-            
-            cv2.imshow("video",frame)
-            c= cv2.waitKey(1) & 0xff 
-            if camera_save_path!="":
-                out.write(frame)
-            
-            # ESC
-            if c==27:
-                capture.release()
-                break
-
-        print("Video Detection Done!")
-        capture.release()
-        if camera_save_path!="":
-            print("Save processed video to the path :" + camera_save_path)
             out.release()
         cv2.destroyAllWindows()
